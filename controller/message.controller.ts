15b1ef8569cf2358f1628bd53989eced6805aed1
Kashish <kashish.biserwal@protonmail.com>
2024-08-07 18:15:45 +0530

    check

--- controller/message.controller.ts
+++ controller/message.controller.ts
@@ -154,6 +154,8 @@ export const addParticipantsToGroup = async (req: ExtendedRequest, res: Response
         const senderId = req.user.id
         const participants = req.body.participants
         const conversationId = req.params.conversationId
+        const sender = await prisma.user.findUnique({ where: { id: req.user.id } });
+        const profile_pic = sender?.image ?? '';
         if(!participants) return res.status(400).send({ message: 'Participants are required' })
         const group = await prisma.conversation.findFirst({
             where: {
@@ -173,13 +175,8 @@ export const addParticipantsToGroup = async (req: ExtendedRequest, res: Response
                     conversationId: Number(conversationId),
                 }
             })
-        })
-        const sender = await prisma.user.findUnique({ where: { id: req.user.id } });
-        const profile_pic = sender?.image ?? '';
-        participants.forEach(async (participant: any) => {
-            console.log(senderId, participant, profile_pic);
             await sendNotif(senderId, participant, profile_pic, 'New Group', `${req.user.username} added you in a group`);
-        });
+        })
         return res.status(200).send({ message: 'Participants added to group' })
     }catch(err){
         return res.status(500).send({ message: 'Error adding participants to group' })
