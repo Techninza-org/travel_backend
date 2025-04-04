bd9c12849d5750d1320decd65284cdf7ad9ca19b
vivekpalv <vivekpalvp0770@gmail.com>
2025-04-04 10:53:06 +0530

    ac

--- controller/user.controller.ts
+++ controller/user.controller.ts
@@ -1512,7 +1512,7 @@ const createItinerary = async (req: ExtendedRequest, res: Response, next: NextFu
 
         // if (!itinerary) { return res.status(404).send({status: 404, message: "invalid itinerary id"}); }
 
-        if (!itinerary_id) {
+        if (!itinerary) {
             const newItinerary = await prisma.itinerary.create({
                 data: {
                     user_id: user.id,
