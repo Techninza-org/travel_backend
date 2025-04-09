2b04a840d333795848331f3d7e032677d8d55b92
vivekpalv <vivekpalvp0770@gmail.com>
2025-04-09 16:21:40 +0530

    t

--- app.ts
+++ app.ts
@@ -573,7 +573,7 @@ const io = new Server(httpsServer, {
         origin: '*',
         methods: ['GET', 'POST'],
     },
-    path: '/socket.io',
+    // path: '/socket.io',
 })
 
 export const getReceiverSocketId = (receiverId: string) => {
