cefe04a8436fd5ea1ef751d3463e450d0d28123a
vivekpalv <vivekpalvp0770@gmail.com>
2025-04-09 17:33:26 +0530

    socket

--- app.ts
+++ app.ts
@@ -575,7 +575,7 @@ const ca = fs.readFileSync('/home/ubuntu/chain.pem', 'utf8')
 
   const credentials = {
     key : fs.readFileSync('/home/ubuntu/privkey.pem', 'utf8'),
-    cert: fs.readFileSync('/home/ubuntu/chain.pem', 'utf8'),
+    cert: fs.readFileSync('/home/ubuntu/fullchain.pem', 'utf8'),
   };
   
 const httpsServer = https.createServer(credentials, app)
