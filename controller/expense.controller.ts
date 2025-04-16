d0ce8b387785d71c237fca04e9b74d050f9d574f
vivekpalv <vivekpalvp0770@gmail.com>
2025-04-16 16:23:36 +0530

    formatting code

--- controller/expense.controller.ts
+++ controller/expense.controller.ts
@@ -5,140 +5,118 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export const CreateExpense = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
-    try{const user = req.user
+    const user = req.user
     const body = req.body
-    if (!helper.isValidatePaylod(body, ['amount', 'category', 'trip_id'])) {
-        return res
-            .status(200)
-            .send({ status: 200, error: 'Invalid payload', error_description: 'trip_id, amount, category is required.' })
+    const { trip_id, amount, category, note } = body
+    
+    if (!helper.isValidatePaylod(body, ['amount', 'category', 'trip_id'])) { return res.status(200).send({ status: 200, error: 'Invalid payload', error_description: 'trip_id, amount, category is required.' }) }
+    if (typeof trip_id !== 'number' || !Number.isInteger(trip_id) || trip_id <= 0) { return res.status(400).send({ status: 400, error: 'Bad Request', error_description: 'Trip id should be a positive integer value', }); }
+    if (typeof amount !== 'number' || !Number.isInteger(amount) || amount <= 0) { return res.status(400).send({ status: 400, error: 'Bad Request', error_description: 'Amount should be a positive integer value', }); }
+    
+    if (typeof category !== 'string') { return res.status(400).send({ status: 400, error: 'Bad Request', error_description: 'Category should be a String', }); }
+    
+    if (note) {
+        if (typeof note !== 'string') { return res.status(400).send({ status: 400, error: 'Bad Request', error_description: 'note should be a String', }); }
     }
-    const {trip_id, amount, category, note} = body
-    if(typeof trip_id !== 'number' || !Number.isInteger(trip_id) || trip_id <= 0){
-        return res.status(400).send({
-            status: 400,
-            error: 'Bad Request',
-            error_description: 'Trip id should be a positive integer value',
-        });
+
+    try {
+
+        const trip = await prisma.trip.findFirst({ where: { id: body.trip_id } })
+        if (!trip) { return res.status(404).send({ status: 404, error: 'Trip not found', error_description: 'Trip not found for the given id.' }) }
+
+        const expense = await prisma.expense.create({
+            data: {
+                amount: body.amount,
+                category: body.category,
+                note: body.note,
+                trip_id: body.trip_id,
+                user_id: user.id,
+            },
+        })
+        return res.status(200).send({ status: 201, message: 'Created', expense: expense })
+    } catch (err) {
+        return next(err)
     }
-    if(typeof amount !== 'number' || !Number.isInteger(amount) || amount <= 0){
-        return res.status(400).send({
-            status: 400,
-            error: 'Bad Request',
-            error_description: 'Amount should be a positive integer value',
-        });
-    }
-    if(typeof category !== 'string'){
-        return res.status(400).send({
-            status: 400,
-            error: 'Bad Request',
-            error_description: 'Category should be a String',
-        });
-    }
-    if(note){
-        if(typeof note !== 'string'){
-            return res.status(400).send({
-                status: 400,
-                error: 'Bad Request',
-                error_description: 'note should be a String',
-            });
-        }
-    }
-    const trip = await prisma.trip.findFirst({ where: { id: body.trip_id } })
-    if (!trip) {
-        return res.status(404).send({ status: 404, error: 'Trip not found', error_description: 'Trip not found for the given id.' })
-    }
-    const expense = await prisma.expense.create({
-        data: {
-            amount: body.amount,
-            category: body.category,
-            note: body.note,
-            trip_id: body.trip_id,
-            user_id: user.id,
-        },
-    })
-    return res.status(200).send({ status: 201, message: 'Created', expense: expense })
-}catch(err){
-    return next(err)
-}
 }
 
 export const GetTripExpenses = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
-    try{
-    let tripId: string | number = req.params.id
-    const user = req.user
-    if (!tripId) {
-        return res
-            .status(200)
-            .send({ status: 400, error: 'Invalid payload', error_description: 'id(trip) is required in params.' })
-    }
-    tripId = Number(tripId)
-    if(typeof tripId !== 'number' || !Number.isInteger(tripId) || tripId <= 0){
-        return res.status(400).send({
-            status: 400,
-            error: 'Bad Request',
-            error_description: 'Trip id should be a positive integer value',
-        });
-    }
-    const expenses = await prisma.expense.findMany({
-        where: { user_id: user.id, trip_id: tripId },
-    })
-    let total = 0
-    expenses.forEach((expense) => {
-        total += expense.amount
-    })
-    
-    return res.status(200).send({ status: 200, expenses: expenses, total: total })
-}catch(err){
-    return next(err)
-}
-}
-
-export const getEachTripsExpenses = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
-   try{ const user = req.user
-    const normal_trips = await prisma.trip.findMany({
-        where: { user_id: user.id, is_payment_confirmed: true},
-        include: {
-            service: {
-                select: {
-                    name: true,
-                    images: true
-                }
-            }
+    try {
+        let tripId: string | number = req.params.id
+        const user = req.user
+        if (!tripId) {
+            return res
+                .status(200)
+                .send({ status: 400, error: 'Invalid payload', error_description: 'id(trip) is required in params.' })
         }
-    })
-    const custom_trips = await prisma.customTrip.findMany({
-        where: { user_id: user.id, is_payment_confirmed: true },
-        include: {
-            service: {
-                select: {
-                    name: true,
-                    images: true
-                }
-            }
+        tripId = Number(tripId)
+        if (typeof tripId !== 'number' || !Number.isInteger(tripId) || tripId <= 0) {
+            return res.status(400).send({
+                status: 400,
+                error: 'Bad Request',
+                error_description: 'Trip id should be a positive integer value',
+            });
         }
-    })
-    const trips = [...normal_trips, ...custom_trips]
-    let tripExpenses = []
-    for (let i = 0; i < trips.length; i++) {
         const expenses = await prisma.expense.findMany({
-            where: { user_id: user.id, trip_id: trips[i].id },
+            where: { user_id: user.id, trip_id: tripId },
         })
         let total = 0
         expenses.forEach((expense) => {
             total += expense.amount
         })
-        tripExpenses.push({ trip: trips[i], total: total })
+
+        return res.status(200).send({ status: 200, expenses: expenses, total: total })
+    } catch (err) {
+        return next(err)
     }
-    let grandTotal = 0;
-    tripExpenses.forEach((tripExpense) => {
-        grandTotal += tripExpense.total;
-    });
-    return res.status(200).send({ status: 200, tripExpenses: tripExpenses, grandTotal: grandTotal });
-}catch(err){
-    return next(err)
-}
 }
 
-const expenseController = {CreateExpense, GetTripExpenses, getEachTripsExpenses}
+export const getEachTripsExpenses = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
+    try {
+        const user = req.user
+        const normal_trips = await prisma.trip.findMany({
+            where: { user_id: user.id, is_payment_confirmed: true },
+            include: {
+                service: {
+                    select: {
+                        name: true,
+                        images: true
+                    }
+                }
+            }
+        })
+        const custom_trips = await prisma.customTrip.findMany({
+            where: { user_id: user.id, is_payment_confirmed: true },
+            include: {
+                service: {
+                    select: {
+                        name: true,
+                        images: true
+                    }
+                }
+            }
+        })
+        const trips = [...normal_trips, ...custom_trips]
+        let tripExpenses = []
+        for (let i = 0; i < trips.length; i++) {
+            const expenses = await prisma.expense.findMany({
+                where: { user_id: user.id, trip_id: trips[i].id },
+            })
+            let total = 0
+            expenses.forEach((expense) => {
+                total += expense.amount
+            })
+            tripExpenses.push({ trip: trips[i], total: total })
+        }
+        let grandTotal = 0;
+        tripExpenses.forEach((tripExpense) => {
+            grandTotal += tripExpense.total;
+        });
+        return res.status(200).send({ status: 200, tripExpenses: tripExpenses, grandTotal: grandTotal });
+    } catch (err) {
+        return next(err)
+    }
+}
+
+const expenseController = { CreateExpense, GetTripExpenses, getEachTripsExpenses }
 
 export default expenseController
