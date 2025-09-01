61796f80e550d845d25ea7b5a6e466f255bc2ab3
Kashish <kashish.biserwal@protonmail.com>
2025-09-01 10:49:50 +0530

    bus apis change

--- controller/bus.controller.ts
+++ controller/bus.controller.ts
@@ -16,7 +16,7 @@ const getSourceCity = async (req: ExtendedRequest, res: Response, next: NextFunc
             sourceKey: sourceKey,
             key: key,
         }
-        const response = await axios.post('http://busapi.easemytrip.com/api/search/GetSourceCity/', requestBody, {
+        const response = await axios.post('http://busapi.easemytrip.com/v1/api/search/GetSourceCity/', requestBody, {
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -36,31 +36,43 @@ const getSourceCity = async (req: ExtendedRequest, res: Response, next: NextFunc
 
 const getSearchList = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
     try {
-        if (!req.body.sourceId || !req.body.key || !req.body.destinationId || !req.body.date || !req.body.isVrl || !req.body.isVolvo) {
-            {
-                return res.status(400).json({
-                    message: 'Please provide sourceId, key, destinationId, isVrl, isVolvo and date',
-                })
-            }
-        }
-        const requestBody = {
-            sourceId: req.body.sourceId,
-            destinationId: req.body.destinationId,
-            key: req.body.key,
-            date: req.body.date,
-            isVrl: req.body.isVrl,
-            isVolvo: req.body.isVolvo,
-        }
-        const response = await axios.post('http://busapi.easemytrip.com/api/detail/List/', requestBody, {
+        // if (!req.body.sourceId || !req.body.key || !req.body.destinationId || !req.body.date || !req.body.isVrl || !req.body.isVolvo) {
+        //     {
+        //         return res.status(400).json({
+        //             message: 'Please provide sourceId, key, destinationId, isVrl, isVolvo and date',
+        //         })
+        //     }
+        // }
+        // const requestBody = {
+        //     sourceId: req.body.sourceId,
+        //     destinationId: req.body.destinationId,
+        //     key: req.body.key,
+        //     date: req.body.date,
+        //     isVrl: req.body.isVrl,
+        //     isVolvo: req.body.isVolvo,
+        // }
+        // const response = await axios.post('http://busapi.easemytrip.com/api/detail/List/', requestBody, {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        // })
+
+        // const data = response.data
+        // return res.status(200).json({
+        //     message: 'Search list fetched successfully',
+        //     data: data,
+        // })
+        const body = req.body;
+
+        const response = await axios.post('http://busapi.easemytrip.com/v1/api/detail/List/', body, {
             headers: {
                 'Content-Type': 'application/json',
             },
         })
 
-        const data = response.data
         return res.status(200).json({
-            message: 'Search list fetched successfully',
-            data: data,
+            message: 'Search bus fetched successfully',
+            data: response.data,
         })
     } catch (err) {
         console.error(err)
@@ -72,34 +84,46 @@ const getSearchList = async (req: ExtendedRequest, res: Response, next: NextFunc
 
 const bindSeat = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
     try{
-        const {id, seater, sleeper, engineId, key, JourneyDate, searchReq, routeid, bpId, dpId, isBpdpLayout, consessionId, singleLeady, stStatus, bustype} = req.body;
+        // const {id, seater, sleeper, engineId, key, JourneyDate, searchReq, routeid, bpId, dpId, isBpdpLayout, consessionId, singleLeady, stStatus, bustype} = req.body;
         
-        const requestBody = {
-            id: id,
-            seater: seater,
-            sleeper: sleeper,
-            engineId: engineId,
-            key: key,
-            JourneyDate: JourneyDate,
-            searchReq: searchReq,
-            routeid: routeid,
-            bpId: bpId,
-            dpId: dpId,
-            isBpdpLayout: isBpdpLayout,
-            consessionId: consessionId,
-            singleLeady: singleLeady,
-            stStatus: stStatus,
-            bustype: bustype
-        }
-        const response = await axios.post('http://busapi.easemytrip.com/api/detail/SeatBind/', requestBody, {
+        // const requestBody = {
+        //     id: id,
+        //     seater: seater,
+        //     sleeper: sleeper,
+        //     engineId: engineId,
+        //     key: key,
+        //     JourneyDate: JourneyDate,
+        //     searchReq: searchReq,
+        //     routeid: routeid,
+        //     bpId: bpId,
+        //     dpId: dpId,
+        //     isBpdpLayout: isBpdpLayout,
+        //     consessionId: consessionId,
+        //     singleLeady: singleLeady,
+        //     stStatus: stStatus,
+        //     bustype: bustype
+        // }
+        // const response = await axios.post('http://busapi.easemytrip.com/api/detail/SeatBind/', requestBody, {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        // })
+        // const data = response.data
+        // return res.status(200).json({
+        //     message: 'Seat bind fetched successfully',
+        //     data: data,
+        // })
+        const body = req.body;
+
+        const response = await axios.post('http://busapi.easemytrip.com/v1/api/detail/SeatBind/', body, {
             headers: {
                 'Content-Type': 'application/json',
             },
         })
-        const data = response.data
+
         return res.status(200).json({
             message: 'Seat bind fetched successfully',
-            data: data,
+            data: response.data,
         })
     }catch(err){
         console.error(err)
@@ -113,7 +137,7 @@ const seatHold = async (req: ExtendedRequest, res: Response, next: NextFunction)
     try{
         const body = req.body;
 
-        const response = await axios.post('http://busapi.easemytrip.com/api/detail/GetTentitiveId/', body, {
+        const response = await axios.post('http://busapi.easemytrip.com/v1/api/detail/GetTentitiveId/', body, {
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -132,26 +156,25 @@ const seatHold = async (req: ExtendedRequest, res: Response, next: NextFunction)
 }
 
 const makeBooking = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
-    const { transactionid, key } = req.body;
-    if (!transactionid || !key) {
-        return res.status(400).json({
-            message: 'Please provide transactionid and key',
-        });
+    try{
+        const body = req.body;
+
+        const response = await axios.post('http://busapi.easemytrip.com/v1/api/detail/MakeBooking', body, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+
+        return res.status(200).json({
+            message: 'Seat book fetched successfully',
+            data: response.data,
+        })
+    }catch(err){
+        console.error(err)
+        return res.status(500).json({
+            message: 'Internal server error',
+        })
     }
-    const requestBody = {
-        transactionid: transactionid,
-        key: key,
-    };
-    const response = await axios.post('http://busapi.easemytrip.com/v1/api/detail/MakeBooking', requestBody, {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    const data = response.data;
-    return res.status(200).json({
-        message: 'Booking made successfully',
-        data: data,
-    });
 }
 
 const cancelTicket = async (req: ExtendedRequest, res: Response, next: NextFunction) => {
